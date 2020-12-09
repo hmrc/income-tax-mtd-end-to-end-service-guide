72e46d9d04c426af65114fa17ec119d2ba43e88c
dwalters <32767569+david-walters-HMRC@users.noreply.github.com>
2020-12-09 12:53:09 +0000

    MTDSA-7550 updated all dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 
 object AppDependencies {
 
-  val bootStrapPlayVersion = "2.0.0"
+  val bootStrapPlayVersion = "3.2.0"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % bootStrapPlayVersion)
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-26" % bootStrapPlayVersion)
 
   val test = Seq(
     "org.pegdown" % "pegdown" % "1.6.0" % "test",
