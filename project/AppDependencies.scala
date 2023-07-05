d058ade381701d96a0497f8ae1694e06627999a1
abigail-houston-hmrc <abigail.houston@digital.hmrc.gov.uk>
2023-07-05 08:27:39 +0100

    Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,7 @@ import sbt._
 object AppDependencies {
 
   val bootStrapPlayVersion = "7.11.0"
-
-  val compile = Seq(ws, "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
+  val compile              = Seq(ws, "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
 
   val test = Seq(
     "org.scalatest"       %% "scalatest"              % "3.2.15"             % "test",
